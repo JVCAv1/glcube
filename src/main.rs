c477d2e57a5ca6d3ed7ccd2933feee31332ddba5
JV-Repo <joao.assumpo@gmail.com>
2022-05-20 01:00:31 -0300

    Changes in relation to lesson 2 of Nercury's opengl tutorial

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+extern crate sdl2;
+extern crate gl;
+
 fn main() {
-    println!("Hello, world!");
+	let sdl = sdl2::init().unwrap();
+	let video_subsystem = sdl.video().unwrap();
+	let _window = video_subsystem
+		.window("glcube", 900, 700)
+		.opengl()
+		.resizable()
+		.build()
+		.unwrap();
+
+	let mut event_pump = sdl.event_pump().unwrap();
+	'main: loop {
+		for event in event_pump.poll_iter() {
+			match event {
+				sdl2::event::Event::Quit { .. } => break 'main,
+				_ => {}
+			}
+		}
+
+		// render window contents here
+	}
 }
