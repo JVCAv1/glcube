2924cab1da3eac67aece196e58d9a1d7b2938deb
João Vitor Cunha Assumpção <joao.assumpo@gmail.com>
2022-08-31 14:42:13 +0000

    Changed to Bevy Engine

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,8 @@
-extern crate sdl2;
-extern crate gl;
+mod startup;
+use bevy::prelude::*;
 
 fn main() {
-	let sdl = sdl2::init().unwrap();
-	let video_subsystem = sdl.video().unwrap();
-	let _window = video_subsystem
-		.window("glcube", 900, 700)
-		.opengl()
-		.resizable()
-		.build()
-		.unwrap();
-
-	let mut event_pump = sdl.event_pump().unwrap();
-	'main: loop {
-		for event in event_pump.poll_iter() {
-			match event {
-				sdl2::event::Event::Quit { .. } => break 'main,
-				_ => {}
-			}
-		}
-
-		// render window contents here
-	}
+	App::new()
+		.add_startup_system(startup())
+		.run();
 }
